c20c555c4fc0bd57968588bbf9a4b0725c6c46e4
Vozniuk Vadym <Wozniukelektric@gmail.com>
2022-11-12 17:08:48 +0200

    endend

--- DZ_9,1.py
+++ DZ_9,1.py
@@ -4,9 +4,20 @@ class Car():
         self.car_color=car_color
         self.car_engine=car_engine
     def drive_forward(self):
-        print('drive_forward')
+        print(self.car_brand+' drive_forward')
     def ride_back(self):
-        print('ride_back')
-audi=Car('audi','a6','2.5')
-audi.drive_forward()
-audi.ride_back()
+        print(self.car_brand+' ride_back')
+
+class Car2(Car):
+    def __init__(self):
+        Car.__init__(self)
+    def turn_left(self):
+        print(self.car_brand+' turn_left')
+    def turn_right(self):
+        print(self.car_brand+' turn_right')
+
+auto=Car('BMW','black','2.5')
+auto.drive_forward()
+auto.ride_back()
+Car2.turn_right(auto)
+Car2.turn_left(auto)
